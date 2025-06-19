1acbe7d21c75ef3f037db01f68692702f4400f0b
NeigelL <neigel.e.lirasan@gmail.com>
2025-06-19 10:28:56 +0800

    * middleware signin test

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -13,17 +13,17 @@ export function middleware(request: NextRequest) {
   //   return NextResponse.next()
   // }
 
-  if (host?.includes(process.env.NEXT_BUYER_DOMAIN || 'buyer.metaland.properties')) {
-    return NextResponse.rewrite(new URL(`/buyer${request.nextUrl.pathname}`, request.url));
-  }
+  // if (host?.includes(process.env.NEXT_BUYER_DOMAIN || 'buyer.metaland.properties')) {
+  //   return NextResponse.rewrite(new URL(`/buyer${request.nextUrl.pathname}`, request.url));
+  // }
 
-  if (host?.includes(process.env.NEXT_ADMIN_DOMAIN || 'app.metaland.properties')) {
-    return NextResponse.rewrite(new URL(`/admin${request.nextUrl.pathname}`, request.url));
-  }
+  // if (host?.includes(process.env.NEXT_ADMIN_DOMAIN || 'app.metaland.properties')) {
+  //   return NextResponse.rewrite(new URL(`/admin${request.nextUrl.pathname}`, request.url));
+  // }
 
-    if (host?.includes(process.env.NEXT_AGENT_DOMAIN || 'agent.metaland.properties')) {
-    return NextResponse.rewrite(new URL(`/agent${request.nextUrl.pathname}`, request.url));
-  }
+  //   if (host?.includes(process.env.NEXT_AGENT_DOMAIN || 'agent.metaland.properties')) {
+  //   return NextResponse.rewrite(new URL(`/agent${request.nextUrl.pathname}`, request.url));
+  // }
 
   return NextResponse.next(); // fallback
 }
