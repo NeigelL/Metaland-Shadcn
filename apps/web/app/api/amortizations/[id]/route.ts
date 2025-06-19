6664e7715a96741581d44a6244e799db00492c09
NeigelL <neigel.e.lirasan@gmail.com>
2025-06-19 13:54:06 +0800

    * fix building

--- apps/web/app/api/amortizations/[id]/route.ts
+++ apps/web/app/api/amortizations/[id]/route.ts
@@ -1,17 +1,15 @@
 
 
 import dbConnect from "@/lib/mongodb"
-import { isLogin } from "@/lib/nextAuthOptions"
 import { getBuyerAmortizationSummaryService } from "@/services/buyerService";
-
 import { NextRequest, NextResponse } from 'next/server'
 
+
 export async function GET(
-//   request: NextRequest,
+  request: NextRequest,
   context: { params: { id: string } } // ✅ This is correct
 ) {
-    await dbConnect()
-
   const { id } = context.params
-  return NextResponse.json(await getBuyerAmortizationSummaryService(id))
+  await dbConnect()
+    return NextResponse.json(await getBuyerAmortizationSummaryService(id))
 }
