5bb8d30882bbefa93a62b0df44f0680358eb8da2
NeigelL <neigel.e.lirasan@gmail.com>
2025-06-19 13:47:12 +0800

    * fix building

--- apps/web/app/api/amortizations/[id]/route.ts
+++ apps/web/app/api/amortizations/[id]/route.ts
@@ -5,21 +5,15 @@ import { isLogin } from "@/lib/nextAuthOptions"
 import { getBuyerAmortizationSummaryService } from "@/services/buyerService";
 import { NextRequest, NextResponse } from "next/server"
 
-const getAmortizationRoute = async(
-      request: NextRequest,
-  { params }: { params: { id: any } }
-) => {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
     await dbConnect()
-
-     const { id } = params
-
+    const { id } = params
     if(!await isLogin()) {
         return new NextResponse("Unauthorized", {status: 401})
     }
 
     return NextResponse.json( await getBuyerAmortizationSummaryService(id) )
-}
-
-export {
-    getAmortizationRoute as GET,
 }
