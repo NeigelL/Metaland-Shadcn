cf6ad931e7b66d13ef33d56a6de5359aa62b1cd1
NeigelL <neigel.e.lirasan@gmail.com>
2025-06-24 16:47:23 +0800

    * update qr-code

--- apps/web/app/buyer/PageClient.tsx
+++ apps/web/app/buyer/PageClient.tsx
@@ -62,7 +62,7 @@ export function PageClient() {
             </DialogHeader>
             <div className="flex justify-center items-center">
               <img
-                src="/images/qr-code.jpg"
+                src="/images/qr-code.png"
                 alt="QR Code"
                 className="w-64 h-64"
               />
