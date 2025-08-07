c2ee971534915461f93542bdb124e83722c5808b
NeigelL <neigel.e.lirasan@gmail.com>
2025-08-07 17:49:07 +0800

    * fix params avoid using destruct

--- apps/web/app/agent/projects/[id]/page.tsx
+++ apps/web/app/agent/projects/[id]/page.tsx
@@ -2,18 +2,22 @@ import { Metadata } from "next"
 import PageClient from "./PageClient";
 import Project from "@/models/projects";
 
-interface Params {
+type Params  = Promise<{
     id: string;
-}
+}>
 
-export default async function Page({ params }: { params: Params }) {
-    const {id} = await params
+export default async function Page(
+    props: { params: Params }
+) {
+    const {id} = await props.params
     console.log("Project ID:", id);
     return <PageClient />;
 }
 
-export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
-    const { id } = await params
+export async function generateMetadata(
+    props: { params: Params }
+): Promise<Metadata> {
+    const { id } = await props.params
     const project  = await Project.findById(id).select( "name address1" )
 
     return {
