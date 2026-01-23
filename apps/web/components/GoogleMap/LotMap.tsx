7f229b96a797659576c0c544be70dcaec448b132
NeigelL <neigel.e.lirasan@gmail.com>
2026-01-23 13:23:20 +0800

    * update buyers maps

--- apps/web/components/GoogleMap/LotMap.tsx
+++ apps/web/components/GoogleMap/LotMap.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { GoogleMap, Marker, Polygon, useJsApiLoader } from "@react-google-maps/api";
-import { useEffect, useState } from "react";
+import { GoogleMap, Marker, OverlayView, Polygon, useJsApiLoader } from "@react-google-maps/api";
+import { useEffect, useMemo, useState } from "react";
 import { Label } from "recharts";
 
 type LatLng = google.maps.LatLngLiteral;
@@ -49,6 +49,13 @@ export default function LotMap({
   const [center, setCenter] = useState<LatLng>({ lat: 0, lng: 0 });
   const [myLots, setMyLots] = useState<any>();
   const [bounds, setBounds] = useState<google.maps.LatLngBounds>();
+  const [mapZoom, setMapZoom] = useState(18)
+
+  const baseFontPx = 9
+  const baseZoom = 18
+  const stepPx = 1.1
+  const minPx = 9
+  const maxPx = 18
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: apiKey,
@@ -69,9 +76,22 @@ export default function LotMap({
     return { lat, lng }
   }
 
+  const isDefaultCenter = (coordinates: any, centerCoordinates: any) => {
+    return coordinates.length > 0 && (centerCoordinates.lat == coordinates[0].lat && centerCoordinates.lng == coordinates[0].lng)
+  }
+
+  function clamp(n: number, min: number, max: number) {
+    return Math.min(min, Math.min(max, n));
+  }
+
+  const fontPx = useMemo(() => {
+    const raw = baseFontPx + (mapZoom - baseZoom) * stepPx;
+    return clamp(raw, minPx, maxPx);
+  }, [mapZoom, baseZoom, baseFontPx, stepPx, minPx, maxPx]);
+
   useEffect(() => {
     const ml = projectPath.lots.filter(
-      (lot: any) => lot.lot_id === lot_id
+      (lot: any) => lot.lot_id?._id === lot_id
     )
     if (!isLoaded || !projectPath) return;
     setProject(projectPath.projects[0].coordinates || []);
@@ -141,19 +161,83 @@ export default function LotMap({
             animation={google.maps.Animation.BOUNCE}
           />
         )}
+
+        {[...blocks, ...others, ...blocks, ...lots, ...misc].map(
+          (polygon: any, key: any) => {
+
+            let block_name = polygon?.block_id?.name?.toUpperCase()?.replace("BLOCK ", "") || ""
+            if (/^\d$/.test(block_name)) {
+              block_name = block_name.padStart(2, "0")
+            }
+
+            let lot_name = polygon?.lot_id?.name?.toUpperCase()?.replace("LOT ", "") || ""
+            if (/^\d$/.test(lot_name)) {
+              lot_name = lot_name.padStart(2, "0")
+            }
+
+            // const showOnZoom = polygon.options?.showZoom == mapZoom
+            const showOnZoom = myLots && myLots._id === polygon?._id || similarLots?.some(lot => lot.lot_id === polygon?.lot_id?._id)
+            return showOnZoom && <OverlayView
+              position={
+                isDefaultCenter(polygon.coordinates, polygon.centerCoordinates) ? getCenterCoordinates(polygon.coordinates) : polygon.centerCoordinates
+              }
+              mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+              key={key + "_label"}
+            >
+              <div
+                style={{
+                  border: "none",
+                  transformOrigin: "center bottom",
+                  textAlign: "center",
+                  width: polygon.type !== "lot" ? "15em" : (fontPx + 20) + "px",
+                  color: polygon?.lot_id?.status == "available" ? "black" : "white",
+                  ...(polygon.type !== "lot"
+                    ? { transform: `rotate(-45deg) scale(${Math.pow(stepPx, mapZoom - baseZoom)})` }
+                    : { transform: `translateZ(0) scale(${Math.pow(stepPx, mapZoom - baseZoom)})` }),
+                }}
+              >
+                {polygon.type == "lot" && showOnZoom && <>
+                  <div>
+                    <span style={{ fontSize: (fontPx + 1) + "px" }}>{[block_name, lot_name].join("-")}</span>
+                  </div>
+
+                  <div>
+                    <span style={{
+                      fontSize: (fontPx + 2) + "px",
+                    }}>
+                      {polygon.lot_id && [polygon.lot_id?.area, 'sqm'].join(" ")}
+                    </span>
+                  </div></>}
+                {
+                  polygon.type == "block" && <div>
+                    <span style={{
+                      color: "black",
+                      fontWeight: "bold",
+                      letterSpacing: "0.5em",
+                      backgroundColor: "white",
+                      // border: "dashed 0.5px black",
+                    }}>
+                      {polygon.label}</span>
+                  </div>
+                }
+              </div>
+            </OverlayView>
+          }
+        )}
+
         {
           [...blocks, ...others, ...blocks, ...lots, ...misc].map(
             (polygon: any, key: any) => {
-              const isSimilarLot = similarLots?.some(lot => lot.lot_id === polygon.lot_id);
+              const isSimilarLot = similarLots?.some(lot => lot.lot_id === polygon?.lot_id?._id);
               const isOverdue = polygon?.lot_id?.amortization_id?.lookup_summary?.status == "OVERDUE"
-              const fillColor = myLots && myLots._id === polygon._id || isSimilarLot ? (isOverdue ? "#000" : "#cf1627") : "transparent";
+              const fillColor = myLots && myLots._id === polygon?._id || isSimilarLot ? (isOverdue ? "#000" : "#cf1627") : "transparent";
               return <Polygon
                 key={key}
                 paths={polygon.coordinates}
                 options={{
                   fillColor: fillColor,
                   strokeColor: "black",
-                  fillOpacity: myLots && myLots._id === polygon._id ? 1 : 0.5,
+                  fillOpacity: 1,
                   strokeOpacity: 0.8,
                   strokeWeight: 0.5,
                   clickable: false,
--- apps/web/services/buyerService.tsx
+++ apps/web/services/buyerService.tsx
@@ -18,16 +18,16 @@ export async function getBuyerLotsService(user_id: string, active: boolean = tru
     await Lot.findOne()
     await Realty.findOne()
     await User.findOne()
-    return await Amortization.find({buyer_ids: user_id, active})
-    .populate([
-        {path:'project_id'},
-        {path:'block_id'},
-        {path:'lot_id'},
-        {path:'realty_id'},
-        {path:'agent_id'},
-        {path:'buyer_ids'},
-        {path:'payment_ids'}
-    ])
+    return await Amortization.find({ buyer_ids: user_id, active })
+        .populate([
+            { path: 'project_id' },
+            { path: 'block_id' },
+            { path: 'lot_id' },
+            { path: 'realty_id' },
+            { path: 'agent_id' },
+            { path: 'buyer_ids' },
+            { path: 'payment_ids' }
+        ])
 }
 
 
@@ -36,26 +36,26 @@ export async function getBuyerLotsDueService(user_id: string, active: boolean = 
     await Project.findOne()
     await Block.findOne()
     await Lot.findOne()
-    return await Amortization.find({buyer_ids: user_id, active})
-    .populate([
-        {path:'project_id', select: "_id name"},
-        {path:'block_id', select: "_id name"},
-        {path:'lot_id', select: "_id name"}
-    ]).select("_id project_id block_id lot_id lookup_summary reference_code")
+    return await Amortization.find({ buyer_ids: user_id, active })
+        .populate([
+            { path: 'project_id', select: "_id name" },
+            { path: 'block_id', select: "_id name" },
+            { path: 'lot_id', select: "_id name" }
+        ]).select("_id project_id block_id lot_id lookup_summary reference_code")
 }
 
-export async function getAmortizationService(amortization_id: String, buyer_id: String, populate :any = [
-    {path:'project_id'},
-    {path:'block_id'},
-    {path:'lot_id'},
-    {path:'realty_id'},
-    {path:'agent_id'},
-    {path:'buyer_ids', populate:{path:'spouse_user_id'}},
-    {path:'buyer_ids.spouse_user_id'},
-    {path:'team_lead'},
-    {path:'team_lead_2'},
-    {path:'agent_id_2'},
-    {path: 'tags'}
+export async function getAmortizationService(amortization_id: String, buyer_id: String, populate: any = [
+    { path: 'project_id' },
+    { path: 'block_id' },
+    { path: 'lot_id' },
+    { path: 'realty_id' },
+    { path: 'agent_id' },
+    { path: 'buyer_ids', populate: { path: 'spouse_user_id' } },
+    { path: 'buyer_ids.spouse_user_id' },
+    { path: 'team_lead' },
+    { path: 'team_lead_2' },
+    { path: 'agent_id_2' },
+    { path: 'tags' }
 ]) {
     await dbConnect()
     await Project.findOne()
@@ -71,11 +71,11 @@ export async function getAmortizationService(amortization_id: String, buyer_id: 
         _id: amortization_id,
         buyer_ids: buyer_id
     })
-    .populate(populate).populate({
-        path: 'payment_ids',
-        populate: {path: 'receiver_account_id acceptable_payment_id verified_by created_by updated_by'},
-        options: {sort: { display_sort: 1}}
-    })
+        .populate(populate).populate({
+            path: 'payment_ids',
+            populate: { path: 'receiver_account_id acceptable_payment_id verified_by created_by updated_by' },
+            options: { sort: { display_sort: 1 } }
+        })
     return amortization
 }
 
@@ -83,7 +83,7 @@ export async function getAmortizationLotMapService(amortization_id: String) {
     await dbConnect()
 
     const amortization = await Amortization.findById(amortization_id).select("project_id")
-    const project_id:any = amortization?.project_id
+    const project_id: any = amortization?.project_id
     const project = await Project.findById(project_id).select("project_map_id")
 
     const query = project?.project_map_id ? {
@@ -93,13 +93,17 @@ export async function getAmortizationLotMapService(amortization_id: String) {
         project_id: project_id
     }
 
-    const polygons = await Polygon.find(query).select("-createdAt -updatedAt")
+    const polygons = await Polygon.find(query).select("-createdAt -updatedAt").populate([
+        { path: 'project_id' },
+        { path: 'block_id' },
+        { path: 'lot_id' }
+    ])
 
-    let groupPolygon : any = {
+    let groupPolygon: any = {
         projects: polygons.filter(p => p.type === 'project'),
         blocks: polygons.filter(p => p.type === 'block'),
         lots: polygons.filter(p => p.type === 'lot'),
-        others: polygons.filter(p => !['lot','block','project'].includes(p.type)),
+        others: polygons.filter(p => !['lot', 'block', 'project'].includes(p.type)),
         misc: polygons.filter(p => p.type === 'misc'),
     }
 
